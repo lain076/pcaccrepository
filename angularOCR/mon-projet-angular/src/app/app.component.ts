515f0223950588e4e414a0e50ffb9fb64d205948
Jad. David Jacquinet <jad@smart.coop>
2018-12-07 15:12:48 +0100

    création premier projet angular

---
+++ angularOCR/mon-projet-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', /* nom UNIQUE donné au component */
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'My awesome app';
+}
---
+++ angularOCR/mon-projet-angular/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MonPremierComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

0aa30858863789eed41ac81f6ac21ab68e1e53b3
perfectBlue <djpcacc@gmail.com>
2022-04-30 16:03:04 +0200

    Add static variable and method

--- Udemy/cs/projets_cs/POO00/programme_poo/programme_poo/Program.cs
+++ Udemy/cs/projets_cs/POO00/programme_poo/programme_poo/Program.cs
@@ -4,16 +4,19 @@ namespace programme_poo
 {
     class person
     {
-
+        private static int _peopleNumber;
         public string name;
         private int age;
-        private string job;        
+        private string job;
+        private int number;
         
         public person(string name, int age, string job)
         {
             this.name = name;
             this.age = age;
             this.job = job;
+            _peopleNumber++;
+            number = _peopleNumber;
         }
 
         public void DisplayInfo()
@@ -21,7 +24,15 @@ namespace programme_poo
             Console.WriteLine($"NAME : {name}");
             Console.WriteLine($"AGE  : {age}");
             Console.WriteLine($"JOB  : {job}");
+            Console.WriteLine($"NUM  : {number}");
+            Console.WriteLine("");
         }
+
+        public static void TotalOfPeople()
+        {
+            Console.WriteLine("Total number of people " + _peopleNumber);
+        }
+            
     }
         
     
@@ -32,15 +43,17 @@ namespace programme_poo
             var people = new List<person>();
 
             people.Add(new person("David", 46, "coder"));
+            people.Add(new person("Samsam", 15, "student"));
             people.Add(new person("Grathy", 42, "cooker"));
-
+            people.Add(new person("Anais", 16, "student"));
+            
             people = people.OrderBy(p =>p.name).ToList();
             
-            foreach (person person in people)
+            foreach (var person in people)
             {
                 person.DisplayInfo();
             }
-            
+            person.TotalOfPeople();
         }
     }
 }
